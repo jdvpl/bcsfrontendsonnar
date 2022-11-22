cd8fafd05184e87003fa090bafe0a42962f7c861
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-11-22 13:29:49 -0500

    test of navtile a commercialAuthorization

--- src/components/commons/index.tsx
+++ src/components/commons/index.tsx
@@ -1,6 +1,4 @@
 export * from './CommercialAuthorization';
-export * from './Footer';
-export * from './Header';
 export * from './Animation';
 export * from './Politicas';
 export * from './Terminos';
