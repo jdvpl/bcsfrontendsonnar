333a7b70aa9bef6bf9a5b2e939851893f3a1f7b6
Camilo Andrés Gonzalez Mendivelso <C2G7M2O1@fs.net>
2022-11-22 13:48:56 -0500

    fix biometry and text

--- src/components/ui/simulation/reviewHouse.tsx
+++ src/components/ui/simulation/reviewHouse.tsx
@@ -22,69 +22,6 @@ export const ReviewHouse: React.FC<ReviewHouseProps> = ({
   monthlyCouteInsurance,
   termFinance,
   rate,
-<<<<<<< HEAD
-  id
-}) => (
-  <div className="flex flex-col items-center ">
-    { lifeInsurance ?
-    <Card
-    className="xs:w-[290px] sm:w-[343px] md:w-[448px]  h-[88px]  bg-[#C4D1DA]  mb-[16px] font-semibold rounded-[8px]"
-    title="Couta mensual total con seguros"
-    value={monthlyCouteInsurance?.toString()} 
-    text="text-[32px] pl-[23px] pt-2 flex items-baseline" 
-    urlsvg=''
-    classtitle="h-[18px] pt-[16px] text-[16px]"
-    subvalue='pesos'
-    textsub='30'
-    tooltiptext=''
-  />:null
-    }
-  {!lifeInsurance ?
-   <Card
-   className="xs:w-[290px] sm:w-[343px] md:w-[448px]  h-[88px]  bg-[#C4D1DA]  mb-[16px] font-semibold rounded-[8px]"
-   title="Couta mensual sin seguros"
-   value={monthlyCoute?.toString()} 
-   text="text-[32px] pl-[23px] pt-2 flex items-baseline" 
-   urlsvg=''
-   classtitle="h-[18px] pt-[16px] text-[16px]"
-   subvalue='pesos'
-   textsub='30'
-   tooltiptext=''
- />:null
-  }
-    
-    <Card
-      className="xs:w-[290px] sm:w-[343px] md:w-[448px]  h-[76px]   bg-[#F3F4F6] pt-[16px] pl-[16px] rounded-[8px] mb-[12px]"
-      title="Valor financiado aproximado"
-      value={financedValue?.toString()}
-      text="text-[20px] pl-[18px] font-semibold flex items-baseline"
-      urlsvg={`${basePath}/images/Money.svg`}
-      classtitle="h-[18px] text-[14px]"
-      subvalue='pesos'
-      textsub='20'
-      tooltiptext=''
-    />
-    <Card
-      className="xs:w-[290px] sm:w-[343px] md:w-[448px]  h-[76px]  bg-[#F3F4F6] pt-[16px] pl-[16px] rounded-[8px] mb-[12px]"
-      title="Plazo"
-      value={termFinance?.toString()}
-      text="text-[20px] pl-[18px] font-semibold"
-      urlsvg={`${basePath}/images/Calendar.svg`}
-      classtitle="h-[14px] text-[14px]"
-      tooltiptext=''
-    />
-    <Card
-      className="xs:w-[290px] sm:w-[343px] md:w-[448px]  h-[76px]    bg-[#F3F4F6] pt-[16px] pl-[16px] rounded-[8px] mb-[12px]"
-      title="Tasa"
-      value={rate}
-      text="text-[20px] pl-[18px] font-semibold"
-      urlsvg={`${basePath}/images/Charts.svg`}
-      classtitle="h-[14px] text-[14px]"
-      tooltip={true}
-      tooltiptext={<p className='text-white'>La tasa de interés es el precio que se debe pagar por utilizar una cantidad de dinero durante un tiempo determinado.<br/>
-        <p className='flex text-white'><p className='font-bold text-white'>Tasa %MV:</p>Tasa nóminal mes vencido</p>
-        <p className='flex text-white'><p className='font-bold text-white'>Tasa EA:</p>Tasa efectiva anual</p>
-=======
   id,
   dataPdf
 }) => {
@@ -151,7 +88,6 @@ export const ReviewHouse: React.FC<ReviewHouseProps> = ({
         tooltiptext={<p className='text-white'>La tasa de interés es el precio que se debe pagar por utilizar una cantidad de dinero durante un tiempo determinado.<br />
           <p className='flex text-white'><p className='font-bold text-white'>Tasa %MV:</p>Tasa nóminal mes vencido</p>
           <p className='flex text-white'><p className='font-bold text-white'>Tasa EA:</p>Tasa efectiva anual</p>
->>>>>>> fe97155472c9abeb5192310f6f23aeda0d6f33c2
         </p>
         }
       />
