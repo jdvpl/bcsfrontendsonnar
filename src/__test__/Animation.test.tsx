11db23eebc6547da178f996784101c179efe1d6d
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-11-24 15:40:04 -0500

    more testing and solving some bugs of canvas

--- src/__test__/Animation.test.tsx
+++ src/__test__/Animation.test.tsx
@@ -1,17 +1,18 @@
 import '@testing-library/jest-dom';
+import 'jest-canvas-mock';
+
 import { render, screen } from '@testing-library/react';
 import Animation from '../components/commons/Animation';
 
 describe('Visibility animation', () => {
   it('appear success message', async () => {
-    render(<Animation show="block" loaded valid />);
-    const successMessage = screen.getByTestId(/success-message/i);
+    render(<Animation show="block" loaded={true} valid={true} />);
+    const successMessage = screen.getByTestId('success-message');
     expect(successMessage).toBeInTheDocument();
   });
-
   it('appear loading message', async () => {
     render(<Animation show="block" loaded={false} valid={false} />);
-    const loadingMessage = screen.getByTestId(/loading-message/i);
+    const loadingMessage = screen.getByTestId('loading-message');
     expect(loadingMessage).toBeInTheDocument();
   });
 });
