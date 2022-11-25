ced5bf218f7f12dbde4bc34f90776fe4859cf883
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-11-25 08:59:33 -0500

    ading some some tests

--- src/__test__/dateOfBirth.test.tsx
+++ src/__test__/dateOfBirth.test.tsx
@@ -1,14 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import DateOfBirth from '../components/ui/inputs/dateOfBirth';
 import userEvent from "@testing-library/user-event";
 
 const mkFn = jest.fn()
 describe('dateOfBirth testing', () => {
-  render(<DateOfBirth id="dateOfBirth"
-    defaultValues={undefined}
-    onChangeDate={mkFn} />)
-  it('should test some component from landing page', () => {
 
-
+  it('should dateOfBirth component rendered', () => {
+    render(<DateOfBirth
+      defaultValues={undefined}
+      onChangeDate={mkFn} />);
+    const component = screen.getByTestId('birth');
+    expect(component.children.length).toBe(3);
   })
 })
