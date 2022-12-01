552bd17ec0ef0df349d31b0ea77671cc43c8b599
Jair Andres Prada Sierra <jpradas@fundaciongruposocial.co>
2022-12-01 16:55:25 +0000

    Merged PR 2343: Adding mobile view in consultancy pages
    # bcs-mortgage-frontend
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [x] Tests for the changes have been added (for bug fixes / features)
    - [x] Docs have been reviewed and added / updated if needed (for bug fixes / features)
    - [x] Build was run locally and any changes were pushed
    - [x] Lint has passed locally and any fixes were made for failures
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [x] :sparkles: Feature
    - [ ] :bug: Bugfix
    - [ ] :pencil2: Fixing typos
    - [x] :lipstick: :art: Code style update (formatting, renaming)
    - [ ] :recycle: Refactoring (no functional changes, no api changes)
    - [ ] :zap: Performance
    - [ ] :pencil: Documentation content changes
    - [ ] :white_check_mark: Unit testing
    - [ ] :green_apple: Fixing something on ios
    - [ ] :robot: Fixing something on android
    - [ ] :ok_hand: Updating due to code review suggest
    - [ ] :rocket: Release: v
    
    ## What is the current module?
    
    <!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
    
    - Before, the design for mobile devices went out of the screen, the model was shown outside the limits of the screen and it had a horizontal focus.
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by this PR. -->
    
    - I have added a mobile view of the interactive query module, now on these devices the buttons are at the bottom of the screen and the detail modal in the center of the screen.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as screenshots of how the component looks before and after the change. -->
    
    Related work items: #7187

--- src/pages/asesoria/index.tsx
+++ src/pages/asesoria/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { FC, ReactNode, useState } from 'react';
 import { basePath } from '../../../next.config';
 import Close from '../../components/svg/Close';
 import LogoBcs from '../../components/svg/LogoBcs';
@@ -10,11 +10,18 @@ import Stepper from '../../components/ui/Stepper';
 import Typography from '../../components/ui/Typography';
 import { initialOptions, stepperTitles } from '../../lib/consultancy';
 import { routes } from '../../routes';
+import { useMediaQuery } from 'react-responsive';
+
+const ConditionalWrapper: FC<any> = ({ condition, wrapper, children }) =>
+  condition ? wrapper(children) : children;
 
 const Consultancy = ({ options = initialOptions }: any) => {
   const [itemActive, setItemActive] = useState('');
   const [activeIndex, setActiveIndex] = useState(0);
   const [actualStep, setActualStep] = useState<number>(1);
+  const isMobile = useMediaQuery({
+    query: '(max-width:720px)',
+  });
   const router = useRouter();
 
   const nextStep = () => {
@@ -68,29 +75,63 @@ const Consultancy = ({ options = initialOptions }: any) => {
         return '10px';
     }
   };
+  const OptionList = () => {
+    return (
+      <>
+        {options[actualStep - 1]?.map((option: any, index: number) => (
+          <Button
+            key={option?.label}
+            onClick={() => openModal(option?.label, index)}
+            variant="secondary"
+            isLanding={`p-0 z-10 md:w-[253px] xs:w-[100%] font-semibold rounded-[8px] lg:h-48px ${
+              itemActive === option?.label ? 'bg-primario-100' : ''
+            }
+            ${itemActive && !isMobile === option?.label ? 'translate-x-[16px]' : ''}
+            `}
+          >
+            <div className="flex justify-center">
+              <span
+                className={`text-center ${
+                  itemActive === option?.label ? 'text-white' : 'text-primario-100'
+                } text-[18px] p-0`}
+              >
+                {option?.label}
+              </span>
+            </div>
+          </Button>
+        ))}
+      </>
+    );
+  };
 
   return (
     <>
       {itemActive !== '' ? (
         <div
-          className="bg-complementario-900/50 w-[100%] h-[300vh]	fixed top-0 left-0"
+          className="bg-complementario-900/50 w-[100%] h-[300vh] fixed top-0 left-0"
           onClick={() => setItemActive('')}
         />
       ) : null}
 
-      <div className="w-[90%] m-auto">
-        <div className="flex justify-between lg:w-[1080px] mx-auto mb-[38px] mt-[64px] lg:mb-[82px] lg:mt-[59px] lg:h-[29px] h-[18px]">
-          <div className="opacity-0 lg:opacity-100">
+      <div className="w-[90%] xs:w-[340px] md:w-[90%] m-auto">
+        <div className="flex justify-between lg:w-[1080px] mx-auto mb-[38px] xs:mt-[36px] md:mt-[64px] lg:mb-[82px] lg:mt-[59px] lg:h-[29px] h-[18px]">
+          <div className="hidden lg:block">
             <LogoBcs />
+          </div>
+          <div className="lg:hidden cursor-pointer">
+            <a href={basePath}>
+              <Icons icon="bcs-arrow-one-left" />
+            </a>
           </div>
           <div className="lg:w-[280px] w-[180px]">
             <LogoForm />
           </div>
         </div>
-        <div className="lg:w-[825px] mx-auto w-[528px]">
+
+        <div className="lg:w-[825px] mx-auto md:w-[528px] w-[full] xs:w-[340px]">
           <Typography
             variant="h2"
-            className="lg:mb-[36px] mb-[48px] text-[28px] text-center"
+            className="lg:mb-[36px] xs:mb-[40px] md:mb-[48px] xs:text-[20px] md:text-[28px] text-center leading-5"
           >
             Esto es lo primero que debe saber para comprar una vivienda
           </Typography>
@@ -98,57 +139,32 @@ const Consultancy = ({ options = initialOptions }: any) => {
             steps={4}
             actualStep={actualStep}
             percentage={40}
-            className="lg:w-[684px] w-[456px] mx-auto lg:mb-[59px] mb-[53px]"
+            className="lg:w-[684px] md:w-[456px] xs:w-full mx-auto lg:mb-[59px] xs:mb-[36px] md:mb-[53px]"
             title={stepperTitles[actualStep - 1]}
           />
         </div>
       </div>
 
-      <div className="lg:w-[1127px] w-[528px] lg:gap-x-[120px] gap-x-[34px] mx-auto flex items-center mb-[77px]">
-        <div onClick={prevStep} className={`cursor-pointer flex items-center flex-col lg:w-[150px]`}>
-          <div className="rounded-full w-[40px] h-[40px] border-primario-20 flex justify-center items-center border-2	mb-[33px]">
-            <Icons
-              icon="bcs-arrow-two-left"
-              iconclassNames="text-[18px] font-bold text-primario-20"
-            />
-          </div>
-          <a className=" text-primario-20 font-bold underline text-center text-[14px]">
-            {actualStep === 1 ? 'Volver al Inicio' : 'Anterior'}
-          </a>
-        </div>
+      <div className="lg:w-[1127px] md:w-[528px] xs:flex-col md:flex-row w-[90%] xs:w-[340px] lg:gap-x-[120px] gap-x-[34px] mx-auto flex items-center mb-[77px]">
+        <img
+          className="w-[100%] xs:max-w-[340px] xs:h-[180px] md:hidden object-contain"
+          src={`${basePath}/images/consultancy/${actualStep}.svg`}
+        />
 
         <div
           className={`${
             itemActive !== '' ? 'sm:ml-[-95px]' : ''
-          }  mx-auto lg:w-[757.2px] w-[600px] h-[279px] lg:m-auto lg:h-[395px] flex flex-col justify-center items-start gap-y-3  box-border`}
+          } md:order-2 hidden  mx-auto lg:w-[757.2px] w-[600px] h-[279px] lg:m-auto lg:h-[395px] md:flex flex-col justify-center items-start gap-y-3 box-border`}
           style={{
-            backgroundImage: `url(${basePath}/images/consultancy/${actualStep}.png)`,
+            backgroundImage: `url(${basePath}/images/consultancy/${actualStep}.svg)`,
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'contain',
             backgroundPositionY: 'center',
-            backgroundPositionX: positionImages(),
+            // backgroundPositionX: positionImages(),
           }}
         >
-          {options[actualStep - 1]?.map((option: any, index: number) => (
-            <Button
-              key={option?.label}
-              onClick={() => openModal(option?.label, index)}
-              variant="secondary"
-              isLanding={`p-0 z-10 w-[253px] font-semibold rounded-[8px] lg:h-48px ${
-                itemActive === option?.label ? 'translate-x-[16px] bg-primario-100' : ''
-              }`}
-            >
-              <div className="flex justify-center">
-                <span
-                  className={`text-center ${
-                    itemActive === option?.label ? 'text-white' : 'text-primario-100'
-                  } text-[18px] p-0`}
-                >
-                  {option?.label}
-                </span>
-              </div>
-            </Button>
-          ))}
+          <OptionList />
+
           {itemActive !== '' ? (
             <div
               onClick={onCloseModal}
@@ -164,20 +180,78 @@ const Consultancy = ({ options = initialOptions }: any) => {
           ) : null}
         </div>
 
-        <div onClick={nextStep} className={`cursor-pointer flex items-center flex-col lg:w-[150px]`}>
-          <div className="rounded-full w-[40px] h-[40px] border-primario-20 flex justify-center items-center border-2 mb-[33px]">
-            <Icons
-              icon="bcs-arrow-two-right"
-              iconclassNames="text-[18px] font-bold text-primario-20"
-            />
+        {itemActive !== '' && isMobile ? (
+          <div className="flex flex-col max-w-[340px] justify-center absolute pb-[50px]">
+            <div
+              onClick={onCloseModal}
+              className="md:hidden w-[100%] rounded-md bg-white z-9 p-[33px] box-border cursor-pointer"
+            >
+              <div className="absolute cursor-pointer rounded-full top-[-70px] right-[50%] translate-x-[50%] p-[6px] text-primario-900 bg-primario-20/30">
+                <div className="rounded-full h-[44.24px] w-[44.24px] bg-primario-20 flex justify-center items-center">
+                  <Close />
+                </div>
+              </div>
+              {renderContent()}
+            </div>
+            <div className="flex flex-col gap-y-3 w-full mt-[12px]">
+              <OptionList />
+            </div>
           </div>
-          <a className=" text-primario-20 font-bold underline text-center text-[14px]">
-            {actualStep === 4 ? 'Salir' : 'Siguiente'}
-          </a>
-        </div>
+        ) : null}
+
+        <ConditionalWrapper
+          condition={isMobile}
+          wrapper={(children: ReactNode) => (
+            <div className="flex justify-between items-center w-full xs:mt-[24px] xs:max-w-[340px]">
+              {children}
+            </div>
+          )}
+        >
+          <>
+            <div
+              onClick={prevStep}
+              className={`md:order-1 cursor-pointer  flex xs:gap-x-3 items-center xs:justify-center xs:flex-row md:flex-col lg:w-[150px]`}
+            >
+              <div className="rounded-full xs:w-[24px] md:w-[40px] xs:h-[24px] md:h-[40px] border-primario-20 flex justify-center items-center border-2 md:mb-[33px]">
+                <Icons
+                  icon="bcs-arrow-two-left"
+                  iconclassNames="md:text-[18px] xs:text-[10px] font-bold text-primario-20"
+                />
+              </div>
+              <a className="text-primario-20 font-bold md:underline text-center text-[14px]">
+                {actualStep === 1 ? 'Volver al Inicio' : 'Anterior'}
+              </a>
+            </div>
+
+            <div
+              onClick={nextStep}
+              className={`md:order-3 cursor-pointer  flex xs:gap-x-3 items-center flex-row md:flex-col lg:w-[150px]`}
+            >
+              <div className="rounded-full xs:w-[24px] md:w-[40px] md:order-1 xs:order-2 xs:h-[24px] md:h-[40px] border-primario-20 flex justify-center items-center border-2 md:mb-[33px]">
+                <Icons
+                  icon="bcs-arrow-two-right"
+                  iconclassNames="md:text-[18px] xs:text-[10px] font-bold text-primario-20"
+                />
+              </div>
+              <a className=" text-primario-20 md:order-2 xs:order-1 font-bold md:underline text-center text-[14px]">
+                {actualStep === 4 ? 'Salir' : 'Siguiente'}
+              </a>
+            </div>
+          </>
+        </ConditionalWrapper>
+
+        {isMobile && itemActive === '' ? (
+          <div className="flex flex-col gap-y-3 w-full mt-10">
+            <OptionList />
+          </div>
+        ) : null}
       </div>
 
-      <div className={`w-full text-center mb-[80px] ${actualStep === 1 || actualStep === 4 ? 'hidden' : ''}`}>
+      <div
+        className={`w-full text-center mb-[80px] xs:hidden md:block${
+          actualStep === 1 || actualStep === 4 ? 'hidden' : ''
+        }`}
+      >
         <a
           href={`${basePath}`}
           className=" text-primario-20 font-bold underline  text-[14px]"
@@ -190,47 +264,3 @@ const Consultancy = ({ options = initialOptions }: any) => {
 };
 
 export default Consultancy;
-
-// interface stepsProps {
-//   steps: number;
-//   actualStep: number;
-//   percentage?: number;
-//   title?: string;
-//   className?: string;
-// }
-// const Stepper = ({
-//   steps,
-//   actualStep,
-//   percentage = 100,
-//   title = '',
-//   className = '',
-// }: stepsProps) => {
-//   const renderSteps = new Array(steps).fill(0);
-//   return (
-//     <div className={className}>
-//       <p
-//         className="text-[16px] leading-4 text-primario-900 font-semibold"
-//         data-testid="titleStep"
-//       >
-//         {title}
-//       </p>
-//       <div className="w-full flex gap-1 my-2">
-//         {renderSteps?.map((item, index) => (
-//           <div
-//             className={`w-[100%] h-[4px] bg-${
-//               actualStep >= index + 1 ? 'primario-20' : 'complementario-80'
-//             }`}
-//           />
-//         ))}
-//       </div>
-//       <p
-//         tabIndex={0}
-//         role="paragraph"
-//         className="text-complementario-100 text-xs ln-16"
-//         data-testid="stepNumber"
-//       >
-//         Paso {actualStep} de {steps}
-//       </p>
-//     </div>
-//   );
-// };
