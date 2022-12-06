d2868eb8abb197908227917cacd79aa82aaccfab
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-12-05 20:03:10 -0500

    registerForm and authentication

--- src/__test__/pages/Authentication.test.tsx
+++ src/__test__/pages/Authentication.test.tsx
@@ -1,9 +1,11 @@
 import 'jest-canvas-mock';
-import { render } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import Authentication from '../../pages/autenticacion';
 import React from 'react'
 import { createMockRouter } from '../utils/createMockRouter';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
+import userEvent from "@testing-library/user-event";
+import { routes } from '../../routes';
 
 describe('Authentication', () => {
   it('should render "Authentication" successfully', () => {
@@ -12,17 +14,28 @@ describe('Authentication', () => {
   });
 
 
-  // test('should goback', async () => {
-  //   const router = createMockRouter({});
-  //   render(
-  //     <RouterContext.Provider value={router}>
-  //       <Authentication />
-  //     </RouterContext.Provider>
-  //   );
-  //   const getbackRouteTest = screen.('getbackRouteTest');
-  //   await waitFor(() => userEvent.click(getbackRouteTest));
+  test('should goback', async () => {
+    const router = createMockRouter({});
+    render(
+      <RouterContext.Provider value={router}>
+        <Authentication />
+      </RouterContext.Provider>
+    );
+    const getbackRouteTest = screen.getByTestId('getbackRouteTest');
+    await waitFor(() => userEvent.click(getbackRouteTest));
+  })
+  test('should goback', async () => {
+    const router = createMockRouter({});
+    render(
+      <RouterContext.Provider value={router}>
+        <Authentication />
+      </RouterContext.Provider>
+    );
+    const identityValidation = screen.getByTestId('btnOnboarding');
+    await waitFor(() => userEvent.click(identityValidation));
+    expect(router.push).toHaveBeenCalledWith(routes.errorValidacion);
+  })
 
-  // })
 
 
 });
