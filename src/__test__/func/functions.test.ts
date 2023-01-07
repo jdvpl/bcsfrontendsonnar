6dd616e19ff9f5ba4e40612c2dd0e00acf1a510a
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-01-06 19:08:47 -0500

    function.test

--- src/__test__/func/functions.test.ts
+++ src/__test__/func/functions.test.ts
@@ -1,18 +1,57 @@
 import { routes } from "../../routes";
 import { onSubmitResponse } from "../../func/functions";
 
-const data = {
-  document_type: 'CC',
-  document_number: '018422010'
-}
-it('redirects to the correct route with error code VQ-02', async () => {
-  // Arrange
-  const router = { push: jest.fn() };
-  const response = { error: true, response: { internal_code: 'VQ-02' } };
-  const sendQuestions = jest.fn(() => Promise.resolve(response));
+describe('onSubmitResponse', () => {
+  let initData: any;
+  let dataTU: any;
+  let router: any;
+  let setDataValid: any;
+  let setDataNumber: any;
+  let sendQuestions: any;
 
-  // Act
-  await onSubmitResponse(data, data, router, jest.fn(), jest.fn());
+  beforeEach(() => {
+    // Inicializar variables aquí
+    initData = {
+      document_type: 'CC',
+      document_number: '12345678'
+    };
+    dataTU = {
+      document_type: 'CC',
+      document_number: '12345678'
+    };
+    router = {
+      push: jest.fn()
+    };
+    setDataValid = jest.fn();
+    setDataNumber = jest.fn();
+    sendQuestions = jest.fn();
+  });
 
-  // Assert
+  it('should navigate to startProccess route if response has error with VQ-01 code', async () => {
+    sendQuestions.mockResolvedValue({
+      error: true,
+      response: {
+        internal_code: 'VQ-01'
+      }
+    });
+
+    await onSubmitResponse(initData, dataTU, router, setDataValid, setDataNumber);
+
+    expect(setDataValid).not.toHaveBeenCalled();
+    expect(setDataNumber).not.toHaveBeenCalled();
+  });
+
+  it('should navigate to validacionErrorValidacionIdentidad route if response has error with VQ-02 code', async () => {
+    sendQuestions.mockResolvedValue({
+      error: true,
+      response: {
+        internal_code: 'VQ-02'
+      }
+    });
+
+    await onSubmitResponse(initData, dataTU, router, setDataValid, setDataNumber);
+
+    expect(setDataValid).not.toHaveBeenCalled();
+    expect(setDataNumber).not.toHaveBeenCalled();
+  });
 });
