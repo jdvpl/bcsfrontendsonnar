49e7d3d3222398bcb918e79bd3da9eda19d141dc
jpradas <J2P7S0R6@fs.net>
2023-01-08 21:12:30 -0500

    add some case test

--- src/__test__/pages/ReviewApplication.test.tsx
+++ src/__test__/pages/ReviewApplication.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
@@ -8,25 +7,22 @@ import { routes } from '../../routes';
 import ResumenSolicitud from '../../pages/resumen-solicitud/index';
 import { createMockRouter } from '../utils/createMockRouter';
 import ReviewApplication from '../../components/ui/application/ReviewApplication';
-import "jest-canvas-mock";
-
+import 'jest-canvas-mock';
 
 describe('<Resumen-Solicitud/>', () => {
   it('should render successfully', () => {
     const { container } = render(<ResumenSolicitud />);
     expect(container).toBeTruthy();
   });
-  // it('Buttom continue', async () => {
-  //   const router = createMockRouter({});
-  //   const { getByTestId } = render(
-  //     <RouterContext.Provider value={router}>
-  //       <ResumenSolicitud />
-  //     </RouterContext.Provider>
-  //   );
-  //   const btnContinue = getByTestId('btn-next');
-  //   await waitFor(() => userEvent.click(btnContinue));
-  //   // expect(router.push).toHaveBeenCalledWith(routes.approvalDataPage);
-  // });
+  it('Buttom continue', async () => {
+    const component = render(
+        <ResumenSolicitud />
+    );
+    const btnContinue = component.getByTestId('btn-next');
+    await waitFor(() => userEvent.click(btnContinue));
+    const messageLoader = component.getByTestId('message-loader');
+    expect(messageLoader).toBeTruthy();  
+  });
   it('Buttom exit', async () => {
     const router = createMockRouter({});
     const { getByTestId } = render(
--- src/components/ui/Loaders/ApplicationLoader.tsx
+++ src/components/ui/Loaders/ApplicationLoader.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
 import Lottie from 'react-lottie-player';
 import Typography from '../Typography';
 import houseAnimation from '../../../lib/houseAnimation.json';
 
 export const ApplicationLoader = () => {
   return (
-    <div className="bg-white fixed top-0 left-0 w-screen h-screen z-10 flex flex-col justify-center items-center">
-      {houseAnimation ? (
-        <Lottie
-          loop
-          animationData={houseAnimation}
-          className="mx-auto xl:w-[378px] xl:mb-8 md:w-[376.91px] md:mb-[26px] sm:w-[321.92px] sm:mb-[17px] mb-6 w-[287.56px]"
-          play
-          speed={0.4}
-        />
-      ) : null}
+    <div
+      className="bg-white fixed top-0 left-0 w-screen h-screen z-10 flex flex-col justify-center items-center"
+      data-testid="message-loader"
+    >
+      <Lottie
+        loop
+        animationData={houseAnimation}
+        className="mx-auto xl:w-[378px] xl:mb-8 md:w-[376.91px] md:mb-[26px] sm:w-[321.92px] sm:mb-[17px] mb-6 w-[287.56px]"
+        play
+        speed={0.4}
+      />
       <Typography
         style={{ fontFamily: 'Montserrat' }}
         variant="bodyM3"
