c467b1b0350c57dc7ed67b812537fa8638429f7b
Camilo Andrés Gonzalez Mendivelso <agonzalezm@fundaciongruposocial.co>
2023-01-10 14:56:39 +0000

    Merged PR 2989: HU82 adjust in screen application review
    # bcs-mortgage-frontend
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ x] Tests for the changes have been added (for bug fixes / features)
    - [ x] Docs have been reviewed and added / updated if needed (for bug fixes / features)
    - [ x] Build was run locally and any changes were pushed
    - [ x] Lint has passed locally and any fixes were made for failures
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [x ] :sparkles: Feature
    - [ ] :bug: Bugfix
    - [ ] :pencil2: Fixing typos
    - [ ] :lipstick: :art: Code style update (formatting, renaming)
    - [ ] :recycle: Refactoring (no functional changes, no api changes)
    - [ ] :zap: Performance
    - [ ] :pencil: Documentation content changes
    - [ ] :white_check_mark: Unit testing
    - [ ] :green_apple: Fixing something on ios
    - [ ] :robot: Fixing something on android
    - [ ] :ok_hand: Updating due to code review suggest
    - [ ] :rocket: Release: v
    
    ## What is the current module?
    
    <!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
    
    - Adjust screen application review, change the titles and add alert banner
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by this PR. -->
    
    -
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as screenshots of how the component looks before and after the change. -->
    
    HU82 adjust in screen application review

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,9 @@ import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const [initialRenderComplete, setInitialRenderComplete] = useState<boolean>(false);
+  const containerTagManager= process.env.TAGMANAGER
   const tagManagerArgs = {
-    gtmId: "GTM-TZ6CJXV",
+    gtmId: containerTagManager || 'GTM-TZ6CJXV',
     dataLayer: {
       userId: "001",
       userProject: "first ID",
@@ -27,24 +28,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const maintenance = process.env.MAINTENANCE || 'false';
   return (
     <>
-    <Script
-        id="manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TZ6CJXV');`,
-        }}
-      />
     <AplicationProvider>
-    <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZ6CJXV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
         {maintenance === 'true' ? <Mantenimiento/> : <Component {...pageProps} />}
     </AplicationProvider>
     </>
