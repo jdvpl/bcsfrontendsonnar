652e27a7d5d1b604d416aa370e58425d26c8eec6
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-01-10 14:36:07 -0500

    h2 and blanck tests

--- src/__test__/components/ui/form/verificationForm.test.tsx
+++ src/__test__/components/ui/form/verificationForm.test.tsx
@@ -1,9 +1,10 @@
+import '@testing-library/jest-dom';
 import { render, fireEvent } from '@testing-library/react';
 import React from 'react'
 import VerificationForm from '../../../../components/ui/Form/verificationForm';
 
 const mkFn = jest.fn()
-describe('Terminos', () => {
+describe('<VerificationForm/>', () => {
   test('should render "VerificationForm" successfully', () => {
     const { baseElement } = render(<VerificationForm onSubmit={mkFn} />);
     expect(baseElement).toBeTruthy();
@@ -23,6 +24,20 @@ describe('Terminos', () => {
     fireEvent.mouseDown(togglePasword)
     fireEvent.click(btnSubmit)
   });
+  test('should render "VerificationForm" button with input blanck', () => {
+    const { getByTestId } = render(<VerificationForm onSubmit={mkFn} />);
+    const inputPass = document.getElementsByName("password")[0];
+    const btnSubmit = getByTestId('btn-save-data');
+    const togglePasword = getByTestId('togglePasword')
+    fireEvent.input(inputPass, { target: { value: '' } })
+    fireEvent.click(togglePasword)
+    fireEvent.mouseDown(togglePasword)
+    fireEvent.click(btnSubmit)
+  });
+  test('should find a tag h2', () => {
+    const { queryByTestId } = render(<VerificationForm onSubmit={mkFn} />);
+    expect(queryByTestId('typographyTest')).toBeInTheDocument()
+  })
 
 
 });
