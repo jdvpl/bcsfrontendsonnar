7274eae080ebe0f6e9db36e111272ac7e6c747b8
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-01-24 15:00:05 -0500

    useRatingModal tests

--- src/__test__/components/ui/modal/ratingModal/useRatingModal.test.tsx
+++ src/__test__/components/ui/modal/ratingModal/useRatingModal.test.tsx
@@ -4,18 +4,30 @@ import React from 'react'
 import '@testing-library/jest-dom';
 
 describe('useRatingModal', () => {
+  beforeEach(() => {
+    const setStateMock = jest.fn();
+    const useStateMock: any = (useState: any) => [useState, setStateMock];
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+  })
   test('onOpenModal event', async () => {
     const { result } = renderHook(() => useRatingModal());
     act(() => {
       result.current.onOpenModal();
     });
-    expect(result.current.isOpen).toBe(false);
+    expect(result.current.isOpen).toBe(true);
+  });
+  test('should initialize isOpen state to true', () => {
+    const { isOpen } = useRatingModal();
+    expect(isOpen).toBe(true);
+  });
+  test('should toggle isOpen state when onOpenModal is called', () => {
+    const { isOpen, onOpenModal } = useRatingModal();
+    onOpenModal();
+    expect(isOpen).toBe(true);
   });
   test('should add "hidden" class to modal element after 1 second', () => {
     jest.useFakeTimers();
-    const setStateMock = jest.fn();
-    const useStateMock: any = (useState: any) => [useState, setStateMock];
-    jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+
     const { onOpenModal } = useRatingModal();
     document.getElementById = jest.fn().mockReturnValue({
       classList: { add: jest.fn() },
