0873b44fe5adc36f721b8b3a59ff759c313e1562
Juan Daniel Suarez Amado <jdsuarez@fundaciongruposocial.co>
2023-01-27 14:02:50 +0000

    Merged PR 3265: onbording texts, popup personal data
    # bcs-mortgage-frontend
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug fixes / features)
    - [ ] Build was run locally and any changes were pushed
    - [ ] Lint has passed locally and any fixes were made for failures
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] :sparkles: Feature
    - [ ] :bug: Bugfix
    - [ ] :pencil2: Fixing typos
    - [ ] :lipstick: :art: Code style update (formatting, renaming)
    - [ ] :recycle: Refactoring (no functional changes, no api changes)
    - [ ] :zap: Performance
    - [ ] :pencil: Documentation content changes
    - [ ] :white_check_mark: Unit testing
    - [ ] :green_apple: Fixing something on ios
    - [ ] :robot: Fixing something on android
    - [ ] :ok_hand: Updating due to code review suggest
    - [ ] :rocket: Release: v
    
    ## What is the current module?
    
    <!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
    
    -
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by this PR. -->
    
    -
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as screenshots of how the component looks before and after the change. -->

--- src/pages/bienvenida/index.tsx
+++ src/pages/bienvenida/index.tsx
@@ -12,15 +12,15 @@ function Bienvenida() {
   useEffect(() => {
     TagManager.dataLayer({
       dataLayer: {
-        event:'load_onboarding',
+        event: 'load_onboarding',
         category: 'load_page',
         action: 'load_onboarding',
         label: 'load_onboarding',
       },
-    }); 
+    });
 
-},[]
-);
+  }, []
+  );
   return (
     <div>
       <Header />
@@ -28,20 +28,20 @@ function Bienvenida() {
         <div className="mt-20 lg:h-[300px] md:w-[292px] md:h-[300px] sm:w-[195px] sm:h-[200px] xs:h-[200px] xs:w-[195px] m-auto ">
           <img src={`${basePath}/images/onboarding.svg`} alt="" />
         </div>
-        <Typography variant='h3' className='text-center mt-[52px] text-primario-900 text-[24px] font-semibold'>
+        <Typography variant='h3' className='text-center mt-[52px] text-primario-900 text-[24px] font-semibold font-poppinsSemiBold  '>
           Cumpla su sueño {""}
           <span className="md:block sm:block xs:block lg:inline">
             de comprar vivienda
           </span>
         </Typography>
-        <Typography variant='h4' className='text-center lg:mt-10 md:mt-3 text-primario-900 text-[18px] font-bold mt-3 sm:font-semibold xs:font-semibold'>Requisitos de solicitud:</Typography>
-        <div className="listInitial flex justify-center">
+        <Typography variant='h4' className='text-center lg:mt-10 md:mt-3 text-primario-900 text-[18px] font-bold mt-3 sm:font-semibold xs:font-semibold font-montserratSemiBold'>Requisitos de solicitud:</Typography>
+        <div className="listInitial flex justify-center font-monserratLight text-[18px] text-primario-900">
           <ul className='md:mt-3'>
-            <li className='mt-3'>Tener entre 18 y 69 años</li>
+            <li className='mt-3'>Tener entre 18 y 70 años</li>
             <li className='mt-3'>Ser asalariado o pensionado</li>
             <li className='mt-3'>Estar al día con sus pagos</li>
             <li className='mt-3'>Pagar salud y pensión</li>
-            <li className='mt-3'>Solicitar individualmente</li>
+            <li className='mt-3'>Solicitar como único titular</li>
           </ul>
         </div>
         <div className="flex justify-center mt-8 mb-11 ">
