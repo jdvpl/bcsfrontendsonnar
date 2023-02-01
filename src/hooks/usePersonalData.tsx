cc923c1db58fe7e9d39f1e0728b7d797f697857b
Juan Daniel Suarez Amado <jdsuarez@fundaciongruposocial.co>
2023-02-01 19:19:53 +0000

    Merged PR 3396: Adding some file to test
    # bcs-mortgage-frontend
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug fixes / features)
    - [ ] Build was run locally and any changes were pushed
    - [ ] Lint has passed locally and any fixes were made for failures
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] :sparkles: Feature
    - [ ] :bug: Bugfix
    - [ ] :pencil2: Fixing typos
    - [ ] :lipstick: :art: Code style update (formatting, renaming)
    - [ ] :recycle: Refactoring (no functional changes, no api changes)
    - [ ] :zap: Performance
    - [ ] :pencil: Documentation content changes
    - [ ] :white_check_mark: Unit testing
    - [ ] :green_apple: Fixing something on ios
    - [ ] :robot: Fixing something on android
    - [ ] :ok_hand: Updating due to code review suggest
    - [ ] :rocket: Release: v
    
    ## What is the current module?
    
    <!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
    
    -
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by this PR. -->
    
    -
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as screenshots of how the component looks before and after the change. -->

--- src/hooks/usePersonalData.tsx
+++ src/hooks/usePersonalData.tsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
+import { calculateAge, isValidDate } from "../utils";
 
-export default function usePersonalData(setValue: any, userInfo: any) {
+export default function usePersonalData(setValue: any, userInfo: any,
+  setError: any,
+  clearErrors: any, dayDt: any,
+  monthDt: any,
+  yearDt: any) {
 
   const date = userInfo.birthDt.split('-');
   useEffect(() => {
@@ -11,5 +16,36 @@ export default function usePersonalData(setValue: any, userInfo: any) {
     setValue('email', userInfo.emailAddr)
     setValue('currentAddress', userInfo.addr1)
   }, [userInfo])
+
+  useEffect(() => {
+    clearErrors('dayDt');
+    if (dayDt && monthDt && yearDt.length === 4) {
+      const age = calculateAge(`${dayDt}/${monthDt}/${yearDt}`);
+      if (age < 19 || age > 71) {
+        setError(
+          'dayDt',
+          {
+            type: 'error',
+            message: 'Fecha inválida',
+          },
+          {
+            shouldFocus: true,
+          }
+        );
+      }
+      if (!isValidDate(parseInt(yearDt), parseInt(monthDt), parseInt(dayDt))) {
+        setError(
+          'dayDt',
+          {
+            type: 'error',
+            message: 'Fecha inválida',
+          },
+          {
+            shouldFocus: true,
+          }
+        );
+      }
+    }
+  }, [yearDt, dayDt, monthDt]);
 
 }
