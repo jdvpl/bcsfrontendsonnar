03cc3fb1ce38778dcade549a0b40be0e9643edd1
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-02-02 16:58:44 -0500

    spaces

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -37,12 +37,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ${isLanding ||
           'w-[288px] sm:w-[343px] sm:max-w-[343px] md:w-[343px] lg:w-[375px] lg:max-w-[375px] '
           }
-        
-           ${variant === 'primary'
+          ${variant === 'primary'
             ? 'py-[10px] text-white bg-primario-100  hover:bg-primario-400  focus:text-secundario-200 focus:shadow-none disabled:text-[#ACD1ED] disabled:bg-gris-80 disabled:shadow-none font-montserratMedium'
             : ''
           }  
-        
         ${variant === 'secondary'
             ? 'valid:text-primario-100  py-[10px]  hover:shadow-none  border-primario-100 bg-white text-primario-100 hover:border-primario-80   focus:text-white focus:shadow-none disabled:text-azul_gris-70 disabled:bg-gris-80 disabled:border-0 border-0 font-montserratMedium'
             : ''
