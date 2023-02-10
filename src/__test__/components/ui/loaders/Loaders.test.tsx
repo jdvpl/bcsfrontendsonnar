fa72ac4d978d00d05e5c83b01ab90580b5c6ba90
jpradas <J2P7S0R6@fs.net>
2023-02-10 11:30:59 -0500

    adding some tex¿st cases

--- src/__test__/components/ui/loaders/Loaders.test.tsx
+++ src/__test__/components/ui/loaders/Loaders.test.tsx
@@ -1,13 +1,27 @@
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 import { render } from '@testing-library/react';
-import React from 'react'
+import React from 'react';
 import { OTLoader } from '../../../../components/ui/Loaders/OTPloader';
+import CheckLoader from '../../../../components/ui/Loaders/CheckLoader';
+import SimulatorLoader from '../../../../components/ui/Loaders/SimulatorLoader';
+import SiteDown from '../../../../pages/validacion/error-servicio';
 
 describe('OTLoader', () => {
   test('should render "OTLoader" successfully', () => {
     const { baseElement } = render(<OTLoader />);
     expect(baseElement).toBeTruthy();
   });
-
+  test('should render "CheckLoader" successfully', () => {
+    const { baseElement } = render(<CheckLoader/>);
+    expect(baseElement).toBeTruthy();
+  });
+  test('should render "SimulatorLoader" successfully', () => {
+    const { baseElement } = render(<SimulatorLoader />);
+    expect(baseElement).toBeTruthy();
+  });
+  test('should render "SimulatorLoader" successfully', () => {
+    const { baseElement } = render(<SiteDown />);
+    expect(baseElement).toBeTruthy();
+  });
 });
--- src/pages/validacion/error-servicio.tsx
+++ src/pages/validacion/error-servicio.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 import { routes } from '../../routes';
 import ErrorLayout from '../../components/ui/error/errorScreen';
 
-export default function siteDown() {
+export default function SiteDown() {
   const router = useRouter();
   return (
     <ErrorLayout
@@ -14,7 +14,7 @@ export default function siteDown() {
       title={<p className='mt-[88px]'>¡Ups! Algo salió mal</p>}
       btnaction={() => router.push(routes.startProccess)}
       text={<p className='lg:mb-[124px] md:mb-[32px] sm:mb-[106px]  xs:mb-[106px] mb-[106px]'>Por favor inténtelo nuevamente</p>}
-      urlsvg={`${basePath}/images/siteDown.svg`}
+      urlsvg={`${basePath}/images/SiteDown.svg`}
       textbtn="Volver a intentar"
       ImgClass="mb-[40px] mt-20 lg:h-[300px] md:w-[340px] md:h-[300px] sm:w-[245px] sm:h-[200px] xs:h-[200px] xs:w-[245px] m-auto pt-[15px]"
     />
