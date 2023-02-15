a4b29588830a7394080711f3116ce009c54c92a6
Jair Andres Prada Sierra <jpradas@fundaciongruposocial.co>
2023-02-15 20:55:19 +0000

    Merged PR 3630: Change texts and font in consultancy page
    # bcs-mortgage-frontend
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [x] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been reviewed and added / updated if needed (for bug fixes / features)
    - [x] Build was run locally and any changes were pushed
    - [ ] Lint has passed locally and any fixes were made for failures
    
    ## Pull request type
    
    <!-- Please do not submit updates to dependencies unless it fixes an issue. -->
    
    <!-- Please try to limit your pull request to one type, submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] :sparkles: Feature
    - [ ] :bug: Bugfix
    - [ ] :pencil2: Fixing typos
    - [ ] :lipstick: :art: Code style update (formatting, renaming)
    - [ ] :recycle: Refactoring (no functional changes, no api changes)
    - [ ] :zap: Performance
    - [ ] :pencil: Documentation content changes
    - [ ] :white_check_mark: Unit testing
    - [ ] :green_apple: Fixing something on ios
    - [ ] :robot: Fixing something on android
    - [ ] :ok_hand: Updating due to code review suggest
    - [ ] :rocket: Release: v
    
    ## What is the current module?
    
    <!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
    
    -
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by this PR. -->
    
    -
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    <!-- If this introduces a breaking change, please describe the impact and migration path for existing applications below. -->
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as screenshots of how the component looks before and after the change. -->
    
    Related work items: #10851

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -9,6 +9,7 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   variant?: 'primary' | 'secondary';
   className?: string;
   isLanding?: string;
+  border?: string;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
@@ -21,6 +22,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       className,
       isLoading,
       isLanding,
+      border = '0',
       ...rest
     },
     ref
@@ -33,18 +35,23 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         type="button"
         disabled={disabled}
         className={`
+        
+
         rounded h-[48px] transition-all duration-500  hover:shadow-media-300  text-lg leading-5
-        ${isLanding ||
+        ${
+          isLanding ||
           'w-[288px] sm:w-[343px] sm:max-w-[343px] md:w-[343px] lg:w-[375px] lg:max-w-[375px] '
-          }
-          ${variant === 'primary'
-            ? 'py-[10px] text-white bg-primario-100  hover:bg-primario-400  focus:text-secundario-200 focus:shadow-none disabled:text-[#ACD1ED] disabled:bg-gris-80 disabled:shadow-none font-montserratMedium'
-            : ''
+        }
+          ${
+            variant === 'primary'
+              ? 'py-[10px] text-white bg-primario-100  hover:bg-primario-400  focus:text-secundario-200 focus:shadow-none disabled:text-[#ACD1ED] disabled:bg-gris-80 disabled:shadow-none font-montserratMedium'
+              : ''
           }  
-        ${variant === 'secondary'
-            ? 'valid:text-primario-100  py-[10px]  hover:shadow-none  border-primario-100 bg-white text-primario-100 hover:border-primario-80   focus:text-white focus:shadow-none disabled:text-azul_gris-70 disabled:bg-gris-80 disabled:border-0 border-0 font-montserratMedium'
+        ${
+          variant === 'secondary'
+            ? `valid:text-primario-100  py-[10px]  hover:shadow-none border-primario-100 bg-white text-primario-100 hover:border-primario-80  border-[${border}px] focus:text-white focus:shadow-none disabled:text-azul_gris-70 disabled:bg-gris-80 disabled:border-0 font-montserratMedium`
             : ''
-          }
+        }
         ${className || ''}
         ${transition || ''}`}
         {...rest}
