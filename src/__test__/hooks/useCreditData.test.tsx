4792e270a464645f1bb7fdab4a1bed3575745533
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-02-27 17:35:00 -0500

    solving some tests

--- src/__test__/hooks/useCreditData.test.tsx
+++ src/__test__/hooks/useCreditData.test.tsx
@@ -1,13 +1,29 @@
 import { act, renderHook } from '@testing-library/react-hooks';
-import useValidations from '../../../../../hooks/useValidationsCreditData';
-import { maxHouseValueNoVis } from '../../../../../lib/simulator';
-import { routes } from '../../../../../routes';
-import { createMockRouter } from '../../../../utils/createMockRouter';
+import useValidations from '../../hooks/useValidationsCreditData';
+import { maxHouseValueNoVis } from '../../lib/simulator';
+import { routes } from '../../routes';
+import { createMockRouter } from '../utils/createMockRouter';
+
+import { riskBoxes } from '../../services';
+import { iCreditData } from '../../interfaces/iCreditData';
+
 const mortgageValues = {}
 const amortizationType = "Pesos";
 const router = createMockRouter({});
-
 jest.useFakeTimers();
+jest.mock('../../services');
+(riskBoxes as jest.Mock).mockReturnValueOnce({
+  response: {
+    status: 403,
+    result: {
+      data: 'ALLOWED',
+    },
+    json: () => {
+      return { internal_code: 'IV-05' };
+    },
+  },
+  error: false,
+});
 describe('useValidations is successfully', () => {
   const houseValue: any = 10000000;
   const financeValue: any = 1000000;
@@ -120,6 +136,97 @@ describe('', () => {
     expect(setCurrentRouting).toHaveBeenCalledWith(routes.finalcialData, false)
     expect(setCurrentRouting).toHaveBeenCalledWith(routes.creditData, false)
   });
+  it('Should render the mortgage value when user gets back', async () => {
+    const setCurrentRouting = jest.fn();
+    const mortgageValuesData: iCreditData = {
+      typeHouse: 'vis',
+      houseStatus: 'new',
+      houseValue: '2000000000',
+      financeValue: '140000000',
+      termFinance: '9',
+      insuranceCheck: 'true',
+      choseOffice: "",
+      office: '',
+      stratum: '3',
+      amortizationType: 'Pesos'
+    }
+    const { result } = renderHook(() => useValidations('novis', 200, 140, 0, jest.fn(), jest.fn(), jest.fn(), jest.fn(), jest.fn(), "", "", null, null, null, router, null, setCurrentRouting, mortgageValuesData, amortizationType));
+
+    await result?.current?.onSubmit()
+    expect(setCurrentRouting).toHaveBeenCalledWith(routes.finalcialData, false)
+    expect(setCurrentRouting).toHaveBeenCalledWith(routes.creditData, false)
+  });
 
 })
 
+
+
+
+
+describe('isValid Method test', () => {
+  const houseValue: any = 10000000;
+  const financeValue: any = 1000000;
+  const termFinance: any = 1000000;
+  const typeHouse: any = 'vis';
+  const houseStatus: any = 'vis';
+  const insuranceCheck: boolean = true;
+  const choseOffice = jest.fn();
+  const office = null;
+  let clearErrors: any;
+  let setError: any;
+  let setPercentageFinance: any;
+  let setValue: any;
+  const stratum: any = 0;
+  const router = {
+    push: jest.fn(),
+  };
+  const houseType = 'vis';
+  const mkFn = jest.fn();
+  const setDataForm = jest.fn();
+  const errors: any = [];
+  beforeEach(async () => {
+    clearErrors = jest.fn();
+    setPercentageFinance = jest.fn();
+    setError = jest.fn();
+    setValue = jest.fn();
+    jest.resetAllMocks();
+  });
+  it('should return true', () => {
+    const { result } = renderHook(() =>
+      useValidations(
+        typeHouse,
+        houseValue,
+        financeValue,
+        termFinance,
+        clearErrors,
+        setError,
+        setPercentageFinance,
+        setValue,
+        setDataForm,
+        houseStatus,
+        insuranceCheck,
+        choseOffice,
+        office,
+        stratum,
+        router,
+        errors,
+        mkFn, mortgageValues, amortizationType
+      )
+    );
+    act(() => {
+      result.current.isValid();
+    });
+
+    act(() => {
+      result.current.automationFinanceValue(100000);
+    });
+
+    act(() => {
+      result.current.automationFinanceValue(0);
+    });
+
+    act(async () => {
+      await result.current.onSubmit();
+    });
+  });
+});
