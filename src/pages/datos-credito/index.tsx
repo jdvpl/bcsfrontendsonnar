48e275e99ba6a89a259a5a244d0e0efd9275c875
jairprada <jairprada9@gmail.com>
2023-03-02 20:32:33 -0500

    adding inactivity wrapper in validation data forms

--- src/pages/datos-credito/index.tsx
+++ src/pages/datos-credito/index.tsx
@@ -3,31 +3,31 @@ import { CreditDataForm } from '../../components/ui/Form/CreditData/CreditDataFo
 import HeaderForm from '../../components/ui/Headers/HeaderForm';
 // eslint-disable-next-line import/no-named-as-default
 import Stepper from '../../components/ui/Stepper';
+import { InactivityWarper } from '../../components/ui/wrapers/InactivityWarper';
 
 function CreditData() {
   return (
-    <div className=" mb-[100px]">
-      <HeaderForm />
-
-      <div className="lg:w-[825px] mx-auto md:w-[528px]  xs:w-[288px] sm:w-[343px] mt-9">
-        <Stepper
-          steps={5}
-          actualStep={4}
-          percentage={100}
-          className="lg:w-[684px] md:w-[528px] xs:w-full mx-auto lg:mb-[64px] xs:mb-[40px] md:mb-[40px]"
-          title="Personalización del crédito"
-        />
+    <InactivityWarper>
+      <div className=" mb-[100px]">
+        <HeaderForm />
+        <div className="lg:w-[825px] mx-auto md:w-[528px]  xs:w-[288px] sm:w-[343px] mt-9">
+          <Stepper
+            steps={5}
+            actualStep={4}
+            percentage={100}
+            className="lg:w-[684px] md:w-[528px] xs:w-full mx-auto lg:mb-[64px] xs:mb-[40px] md:mb-[40px]"
+            title="Personalización del crédito"
+          />
+        </div>
+        <h2 className="text-center font-poppinsSemiBold md:leading-[30px] xs:leading-[22px] mb-[52px] md:text-[28px] xs:text-[20px] font-semibold text-primario-900">
+          Defina las condiciones <br className="lg:hidden xs:block" />
+          de su crédito
+        </h2>
+        <div className="w-[288px] xs:w-[288px] sm:w-[343px] md:w-[550px] lg:w-[686px] mx-auto">
+          <CreditDataForm />
+        </div>
       </div>
-
-      <h2 className="text-center font-poppinsSemiBold md:leading-[30px] xs:leading-[22px] mb-[52px] md:text-[28px] xs:text-[20px] font-semibold text-primario-900">
-        Defina las condiciones <br className="lg:hidden xs:block" />
-        de su crédito
-      </h2>
-
-      <div className="w-[288px] xs:w-[288px] sm:w-[343px] md:w-[550px] lg:w-[686px] mx-auto">
-        <CreditDataForm />
-      </div>
-    </div>
+    </InactivityWarper>
   );
 }
 
--- src/pages/datos-personales/index.tsx
+++ src/pages/datos-personales/index.tsx
@@ -7,6 +7,7 @@ import Advisory from '../../components/commons/Advisory';
 import Stepper from '../../components/ui/Stepper';
 import PersonalDataBasic from '../../components/ui/Form/PersonalDataBasic';
 import HeaderForm from '../../components/ui/Headers/HeaderForm';
+import { InactivityWarper } from '../../components/ui/wrapers/InactivityWarper';
 
 function PersonalData() {
   const [showModal, setShowModal] = useState<boolean>(true);
@@ -36,38 +37,38 @@ function PersonalData() {
   }
   const keyModal = 'advisoryType' in dataUser;
   return (
-    <div data-testid="PersonalDataTest" >
-      {showModal && !keyModal && (
-        <Modal
-          showModal={showModal}
-          onClose={() => closeModal()}
-          compont={componentModal}
-          advisory
-          heightModal="lg:h-[50%]"
-        />
-      )}
-      <HeaderForm />
-
-      <div className="lg:w-[684px] md:w-[528px] sm:w-[343px] w-[288px] m-auto mt-9">
-        <Stepper
-          steps={5}
-          actualStep={2}
-          percentage={50}
-          className="lg:w-[684px] md:w-[528px] xs:w-full mx-auto lg:mb-[59px] xs:mb-[36px] md:mb-[53px] text-left"
-          title="Datos personales"
-        />
-        <p className="md:mt-[64px] mt-[40px] leading-8 text-center font-poppinsSemiBold font-semibold md:text-[28px] sm:text-[20px] mx-auto lg:w-full md:w-[400px]">
-          {dataPersonalInfo.firstName || 'Hola'},{' '}
-          {dataPersonalInfo.isClient
-            ? 'confirme sus datos personales'
-            : 'ingrese sus datos personales'}
-        </p>
-
-        <div className="flex gap-1 md:mt-[52px] mt-[36px] lg:w-[684px] md:w-[528px] sm:w-[343px] w-[288px] mx-auto">
-          <PersonalDataBasic userInfo={dataPersonalInfo} />
+    <InactivityWarper>
+      <div data-testid="PersonalDataTest" >
+        {showModal && !keyModal && (
+          <Modal
+            showModal={showModal}
+            onClose={() => closeModal()}
+            compont={componentModal}
+            advisory
+            heightModal="lg:h-[50%]"
+          />
+        )}
+        <HeaderForm />
+        <div className="lg:w-[684px] md:w-[528px] sm:w-[343px] w-[288px] m-auto mt-9">
+          <Stepper
+            steps={5}
+            actualStep={2}
+            percentage={50}
+            className="lg:w-[684px] md:w-[528px] xs:w-full mx-auto lg:mb-[59px] xs:mb-[36px] md:mb-[53px] text-left"
+            title="Datos personales"
+          />
+          <p className="md:mt-[64px] mt-[40px] leading-8 text-center font-poppinsSemiBold font-semibold md:text-[28px] sm:text-[20px] mx-auto lg:w-full md:w-[400px]">
+            {dataPersonalInfo.firstName || 'Hola'},{' '}
+            {dataPersonalInfo.isClient
+              ? 'confirme sus datos personales'
+              : 'ingrese sus datos personales'}
+          </p>
+          <div className="flex gap-1 md:mt-[52px] mt-[36px] lg:w-[684px] md:w-[528px] sm:w-[343px] w-[288px] mx-auto">
+            <PersonalDataBasic userInfo={dataPersonalInfo} />
+          </div>
         </div>
       </div>
-    </div>
+    </InactivityWarper>
   );
 }
 
