8870bb0c2dc5ed1ab6d26a5c57c551b439093ffb
Jair Andres Prada Sierra <jpradas@fundaciongruposocial.co>
2023-03-10 11:36:17 -0500

    change style fonts in forms

--- src/pages/simulador/index.tsx
+++ src/pages/simulador/index.tsx
@@ -8,7 +8,7 @@ import useSimulator from './useSimulator';
 import TagManager from 'react-gtm-module';
 
 function Simulator() {
-  const { simulatioTypeOption, setsimulatioTypeOption, onSubmit,isLoading } = useSimulator();
+  const { simulatioTypeOption, setsimulatioTypeOption, onSubmit, isLoading } = useSimulator();
   useEffect(() => {
     TagManager.dataLayer({
       dataLayer: {
@@ -31,28 +31,25 @@ function Simulator() {
           <LogoForm />
         </div>
       </div>
-      <div className="lg:w-[684px] md:w-[584px] w-[343px] m-auto">
-        <Typography variant="h2" className="mt-8 text-center">
+      <div className="lg:w-[760px] md:w-[584px] w-[343px] m-auto">
+        <h2 className="mt-8 text-center font-poppinsSemiBold font-semibold md:text-[28px] text-[20px]">
           Simulador del crédito
-        </Typography>
+        </h2>
         <div>
-          <Typography
-            variant="bodyS3"
-            className="text-center mt-[40px] md:mt-[54px] lg:mt-[52px] pmx-3 text-primario-900"
-          >
+          <p className="text-center mt-2 pmx-3 text-primario-900 font-montserratRegular font-normal">
             A través de nuestro simulador podrá realizar los cálculos necesarios para
             conocer los valores aproximados relacionados con el crédito de vivienda con
             amortización en pesos.
-          </Typography>
+          </p>
         </div>
-        <div className="flex gap-1 my-8 w-[343px] md:w-[517px] xl:w-[656px] mx-auto">
+        <div className="flex gap-1 md:mb-6  mt-8 lg:mt-[52px] w-[343px] md:w-[517px] xl:w-[656px] mx-auto">
           <button
             data-testid="houseTestBtn"
             type="button"
             className={
               simulatioTypeOption === 'house'
-                ? ` font-semibold button-shadow text-[14px] w-full max-w-[23.438rem] rounded-l-lg h-[3rem] transition-all duration-500 text-white bg-primario-100 hover:bg-primario-400  shadow-none`
-                : ` font-semibold button-shadow text-[14px] w-full max-w-[23.438rem] rounded-l-lg h-[3rem] text-complementario-70 bg-gris-90 shadow-none`
+                ? `font-montserratRegular font-medium button-shadow text-[12px] leading-[14px] w-full max-w-[23.438rem] rounded-l-lg h-[3rem] transition-all duration-500 text-white bg-primario-100 hover:bg-primario-400  shadow-none`
+                : `font-montserratRegular font-medium button-shadow text-[12px] leading-[14px] w-full max-w-[23.438rem] rounded-l-lg h-[3rem] text-complementario-70 bg-gris-90 shadow-none`
             }
             onClick={() => setsimulatioTypeOption('house')}
           >
@@ -63,8 +60,8 @@ function Simulator() {
             type="button"
             className={
               simulatioTypeOption === 'salary'
-                ? ` font-semibold button-shadow text-[14px] w-full max-w-[23.438rem] rounded-r-lg h-[3rem] transition-all duration-500 text-white bg-primario-100 hover:bg-primario-400  shadow-none`
-                : ` font-semibold button-shadow text-[14px] w-full max-w-[23.438rem] rounded-r-lg h-[3rem] text-complementario-70 bg-gris-90 shadow-none`
+                ? ` font-montserratRegular font-medium button-shadow text-[12px] leading-[14px] w-full max-w-[23.438rem] rounded-r-lg h-[3rem] transition-all duration-500 text-white bg-primario-100 hover:bg-primario-400  shadow-none`
+                : ` font-montserratRegular font-medium button-shadow text-[12px] leading-[14px] w-full max-w-[23.438rem] rounded-r-lg h-[3rem] text-complementario-70 bg-gris-90 shadow-none`
             }
             onClick={() => setsimulatioTypeOption('salary')}
           >
