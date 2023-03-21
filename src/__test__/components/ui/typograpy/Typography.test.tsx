1230d4e2546e4101b5ef0bb868613e0c103bf643
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-03-21 15:15:44 -0500

    adding some tests

--- src/__test__/components/ui/typograpy/Typography.test.tsx
+++ src/__test__/components/ui/typograpy/Typography.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react'
 import Typography from '../../../../components/ui/Typography';
+import '@testing-library/jest-dom';
 
 describe('Typography', () => {
   test('should render "Typography" successfully', () => {
@@ -16,6 +17,25 @@ describe('Typography', () => {
     render(<Typography variant='bodyM1' typeFont='Bold'>Hola mundo</Typography>);
     const element = screen.getByTestId('typographyTest')
     expect(element.tagName).toMatch('P');
+  });
+  it('renders the children correctly', () => {
+    render(<Typography variant='bodyM3'>Example text</Typography>);
+    expect(screen.getByTestId('typographyTest')).toHaveTextContent('Example text');
+  });
+
+  it('applies the correct variant class', () => {
+    render(<Typography variant="h1">Example text</Typography>);
+    expect(screen.getByTestId('typographyTest')).toHaveClass('text-32px leading-36px');
+  });
+
+  it('applies the correct font class', () => {
+    render(<Typography variant="bodyM1" typeFont="Bold">Example text</Typography>);
+    expect(screen.getByTestId('typographyTest')).toHaveClass('font-montserratSemiBold');
+  });
+
+  it('applies additional classnames correctly', () => {
+    render(<Typography className="additional-class" variant='bodyM3'>Example text</Typography>);
+    expect(screen.getByTestId('typographyTest')).toHaveClass('additional-class');
   });
 
 });
