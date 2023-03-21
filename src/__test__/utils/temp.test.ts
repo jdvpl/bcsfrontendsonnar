6b434b98cad4b350eae4843bd57c3564850e4e65
Jair Andres Prada Sierra <jpradas@fundaciongruposocial.co>
2023-03-21 07:40:53 -0500

    solving test conflicts

--- src/__test__/utils/temp.test.ts
+++ src/__test__/utils/temp.test.ts
@@ -1,4 +1,4 @@
-import { testDefault,testDefault2 } from "../../utils/temp"
+import { testDefault,testDefault2,testDefault3,testDefault4,testDefault5,testDefault6,testDefault7,testDefault8,testDefault9,testDefault10 } from "../../utils/temp"
 
 describe("",()=>{
 test("",()=>{
@@ -8,4 +8,37 @@ test("",()=>{
 test("",()=>{
     expect(testDefault2()).toMatch("opt")
 })
+test("",()=>{
+    expect(testDefault3()).toBe(3)
+
+})
+test("",()=>{
+    expect(testDefault4()).toBe(4)
+
+})
+test("",()=>{
+    expect(testDefault5()).toBe(5)
+
+})
+test("",()=>{
+    expect(testDefault6()).toBe(6)
+
+})
+test("",()=>{
+    expect(testDefault7()).toBe(7)
+
+})
+test("",()=>{
+    expect(testDefault8()).toBe(8)
+
+})
+test("",()=>{
+    expect(testDefault9()).toBe(9)
+
+})
+test("",()=>{
+    expect(testDefault10()).toBe(10)
+
+})
+
 })
--- src/utils/temp.ts
+++ src/utils/temp.ts
@@ -5,3 +5,30 @@ export const testDefault =()=>{
 export const testDefault2=()=>{
     return "opt"
 }
+export const testDefault3 =()=>{
+    return 3
+}
+export const testDefault4 =()=>{
+    return 4
+}
+export const testDefault5 =()=>{
+    return 5
+}
+export const testDefault6 =()=>{
+    return 6
+}
+export const testDefault7 =()=>{
+    return 7
+}
+export const testDefault8 =()=>{
+    return 8
+}
+export const testDefault9 =()=>{
+    return 9
+}
+export const testDefault10 =()=>{
+    return 10
+}
+
+
+// porfa copie unas 
