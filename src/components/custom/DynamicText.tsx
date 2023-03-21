5cc18c28463a220a3ce5743a87723c8113c64498
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-03-21 15:57:57 -0500

    removing test

--- src/components/custom/DynamicText.tsx
+++ src/components/custom/DynamicText.tsx
@@ -4,7 +4,7 @@ import Typography from '../ui/Typography'
 const DynamicText = ({ isBrowser }: any) => {
   return (
     <div>
-      {isBrowser ? <Typography variant='bodyM3' typeFont='Light' className='text-center mt-3' data-testid="phoneFromTest" role="phoneFromTest">
+      {isBrowser ? <Typography variant='bodyM3' typeFont='Light' className='text-center mt-3'>
         Sugerimos realizar este proceso
         <span className="block">
           desde un celular
