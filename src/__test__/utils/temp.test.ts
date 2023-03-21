13f1f328f54d0682d1c84200a190edd255202bc4
Jair Andres Prada Sierra <jpradas@fundaciongruposocial.co>
2023-03-21 07:06:27 -0500

    addinh new test cases

---
+++ src/__test__/utils/temp.test.ts
@@ -0,0 +1,11 @@
+import { testDefault,testDefault2 } from "../../utils/temp"
+
+describe("",()=>{
+test("",()=>{
+    expect(testDefault()).toMatch("option default to test")
+
+})
+test("",()=>{
+    expect(testDefault2()).toMatch("opt")
+})
+})
---
+++ src/utils/temp.ts
@@ -0,0 +1,7 @@
+export const testDefault =()=>{
+    return "option default to test"
+}
+
+export const testDefault2=()=>{
+    return "opt"
+}
