e1e264c57fa625588e8a515d80ec5bb7b7450376
jpradas <jpradas@fundaciongruposocial.co>
2023-03-30 16:47:08 -0500

    solving test

--- src/config/AxiosMortgage.ts
+++ src/config/AxiosMortgage.ts
@@ -5,4 +5,4 @@ export const clientAxiosBackend = axios?.create({
   baseURL: process.env.APIURLMORTGAGE,
 });
 
-clientAxiosBackend.interceptors.response.use(handlerDecrypt, axiosErrorMiddleware);
+clientAxiosBackend?.interceptors?.response.use(handlerDecrypt, axiosErrorMiddleware);
