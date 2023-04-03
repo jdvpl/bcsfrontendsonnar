af65437979d508c57da2ca88c004748ffd62ca72
Jair Andres Prada Sierra <jpradas@fundaiongruposocial.co>
2023-04-03 11:45:30 -0500

    solving pdf problems

--- src/components/commons/Politicas.tsx
+++ src/components/commons/Politicas.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { basePath } from '../../../next.config';
 import { pdfjs, Document, Page } from 'react-pdf';
 import { FileLoader } from '../ui/Loaders/FileLoader';
-pdfjs.GlobalWorkerOptions.workerSrc = `../../vivienda/files/pdf-worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = "../../vivienda/files/pdf-worker.js";
 
 function Politicas() {
   return (
-    <div data-testid="terminos" className="w-full  relative h-[70vh]">
-      <Document file={`${basePath}/files/ATDP.pdf`} loading={<FileLoader />}>
-        <Page pageNumber={1} renderAnnotationLayer={false} renderTextLayer={false} />
-        <Page pageNumber={2} renderAnnotationLayer={false} renderTextLayer={false} />
-        <Page pageNumber={3} renderAnnotationLayer={false} renderTextLayer={false} />
+    <div data-testid="terminos" className="w-full relative h-[65vh] overflow-x-hidden overflow-y-auto pdf-viwer">
+      <Document renderMode="svg" file={`${basePath}/files/ATDP.pdf`} loading={<FileLoader />} >
+        <Page debug  pageNumber={1}  renderAnnotationLayer={false} renderTextLayer={false} />
+        <Page pageNumber={2}  renderAnnotationLayer={false}  renderTextLayer={false} />
+        <Page pageNumber={3}  renderAnnotationLayer={false}  renderTextLayer={false} />
       </Document>
     </div>
   );
--- src/components/ui/Loaders/FileLoader.tsx
+++ src/components/ui/Loaders/FileLoader.tsx
@@ -3,7 +3,7 @@ import Typography from '../Typography';
 
 export const FileLoader = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center flex-col gap-5">
+    <div className="w-full h-[65vh] flex justify-center items-center flex-col gap-5">
       <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-primario-900"></div>
       <Typography variant="caption2" componentHTML="p" className="font-semibold">
         ... Cargando PDF
